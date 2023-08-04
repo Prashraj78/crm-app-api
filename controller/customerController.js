6717ae8e6a413cadc4fe92cc6f39a39365d99b3a
Prashant Patil <prashantrajputaaaa@gmail.com>
2023-08-04 13:10:32 +0530

    put api

--- controller/customerController.js
+++ controller/customerController.js
@@ -1,6 +1,4 @@
-
 const customers= [
-  
     {
         "name":"Infosys",
         "website":"www.infosys.com",
@@ -16,62 +14,25 @@ const customers= [
         "employees":"300000",
         "year":"1995",
         "turnover":"200000000"
-    },
-    {
-        "name": "Accenture",
-        "website": "www.accenture.com",
-        "ceo": "Uknown",
-        "employees": "2330894",
-        "year": "1878",
-        "turnover": "53000000"
     }
-
-  ];
+];
 
 module.exports.get = (req, res)=>{
-    const page = req.params.page;
-    const obj = {
-        "records": customers.slice((page-1)*100,100*page),
-        "totalCount":customers.length
-    }
-    res.status(200).send(obj);
-}
-
-module.exports.getByName = (req, res)=>{
-  const name = req.params.name;
-  const result = customers.find(c=> c.name==name);
-  if(!result){
-    return res.status(404).send();
-  }
-  return res.status(200).send(result);
+    res.status(200).send(customers);
 }
 
 module.exports.add = (req, res)=>{
     const customer = req.body;
-    customers.push(customer);
-    res.status(200).send(customers);
-}
-
-
-module.exports.getAll = (req, res)=>{
-  res.status(200).send(customers);
-}
-
-
-module.exports.update = (req, res)=>{
-    const customer = req.body;
-    let foundCustomerIndex = customers.findIndex(c=> c._id==customer._id);
-    console.log(foundCustomerIndex);
     console.log(customer);
-    customers[foundCustomerIndex]=customer;
-    res.status(200).send(customers);
-}
-
-module.exports.delete = (req, res)=>{
-    const name = req.params.name;
-    let foundCustomerIndex = customers.findIndex(c=> c.name==name);
-    customers.splice(foundCustomerIndex, 1);
+    customers.push(customer);
     console.log(customers);
     res.status(200).send(customers);
 }
 
+module.exports.update = (req, res)=>{
+    const customer = req.body;
+    let foundCustomerIndex = customers.findIndex(c=> c.name==customer.name);
+    customers[foundCustomerIndex]=customer;
+    console.log(customers);
+    res.status(200).send(customers);
+}
