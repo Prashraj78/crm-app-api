538eb670ae76dcb94eba31f3939b07059580cc00
Prashant Patil <prashantrajputaaaa@gmail.com>
2023-08-12 15:05:35 +0530

    debugiing

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-
 const express = require("express");
 const bodyparser = require("body-parser");
 const customerRoutes = require("./routes/customerRoutes");
 const userRoutes = require("./routes/userRoutes");
-const ticketRoutes = require("./routes/ticketRoutes");
 const server = express();
 const cors = require("cors");
 const mongodb = require("./config/mongodb");
-
 
 server.listen(4000);
 mongodb.connect();
@@ -23,7 +20,7 @@ server.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions
+    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
@@ -34,7 +31,6 @@ server.use(cors());
 
 server.use("/api/customer",customerRoutes.router);
 server.use("/api/user",userRoutes.router);
-server.use("/api/ticket",ticketRoutes.router);
 
 server.get("/", (req, res)=>{
     res.send("Welcome to CRM API");
